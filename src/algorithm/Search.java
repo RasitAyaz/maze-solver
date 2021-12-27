5b95e753aa414ee43bba642161892a93d5930ec2
RasitAyaz <rasitayaz1358@gmail.com>
2021-12-27 15:05:55 +0300

    Add expanded array

--- src/algorithm/Search.java
+++ src/algorithm/Search.java
@@ -9,6 +9,7 @@ import src.Tile;
 public abstract class Search {
     protected List<Coordinate> solutionCoordinates;
     protected List<Tile> exploredTiles;
+    protected List<Coordinate> expandedCoordinates;
     protected double solutionCost;
     protected Maze maze;
     protected List<Tile> frontier;
